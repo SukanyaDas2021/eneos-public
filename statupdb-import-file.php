52032a9ec25bf9f883694323a0016a6d29f8d64f
balakumar23591 <balakumar23591@gmail.com>
2024-01-27 07:40:11 +0530

    ui changes

--- statupdb-import-file.php
+++ statupdb-import-file.php
@@ -22,21 +22,21 @@ foreach ($_POST['choose'] as $key => $value) {
 
     // print_r($compnay_data);
 
-    $name = dataFormating($compnay_data['name']);
-    $industry = dataFormating($compnay_data['Category/Industry']);
-    $address = dataFormating($compnay_data['Company Address']);
+    $name = (isset($_POST['name'][$value]) && !empty($_POST['name'][$value])) ? $_POST['name'][$value] : dataFormating($compnay_data['name']);
+    $industry = (isset($_POST['industry'][$value]) && !empty($_POST['industry'][$value])) ? $_POST['industry'][$value] : dataFormating($compnay_data['Category/Industry']);
+    $address = (isset($_POST['address'][$value]) && !empty($_POST['address'][$value])) ? $_POST['address'][$value] : dataFormating($compnay_data['Company Address']);
     $competitors = dataFormating($compnay_data['Competitors']);
-    $full_description = dataFormating($compnay_data['Full Description']);
+    $full_description = (isset($_POST['full_description'][$value]) && !empty($_POST['full_description'][$value])) ? $_POST['full_description'][$value] : dataFormating($compnay_data['Full Description']);
     $latest_round_data = dataFormating($compnay_data['Latest Round Data']);
     $short_description = dataFormating($compnay_data['Short Description']);
 
     $tag = dataFormating($compnay_data['Tags']);
     $total_funding = dataFormating($compnay_data['Total Funding']);
     $founded_year = dataFormating($compnay_data['Year Founded']);
-    $url = dataFormating($compnay_data['url']);
+    $url = (isset($_POST['url'][$value]) && !empty($_POST['url'][$value])) ? $_POST['url'][$value] : dataFormating($compnay_data['url']);
 
     $created_at = date('Y-m-d');
-    $in_charge = NULL;
+    $in_charge = (isset($_POST['in_charge'][$value]) && !empty($_POST['in_charge'][$value])) ? $_POST['in_charge'][$value] : NULL;
     $source = NULL;
     $final_progress = NULL;
     $current_status = NULL;
