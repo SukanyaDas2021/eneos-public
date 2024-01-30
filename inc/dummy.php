c5d085fa43080d91ea4e5500f3a148d17815c6c0
balakumar23591 <balakumar23591@gmail.com>
2024-01-30 21:33:22 +0530

    dummy content added

--- inc/dummy.php
+++ inc/dummy.php
@@ -1,6 +1,6 @@
 <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<div class="alert alert-warning dummy-alert" role="alert">
+<div class="alert alert-warning dummy-alert mb-0" role="alert">
     <i class="fa-solid fa-triangle-exclamation"></i>
     <b>This page contains only dummy data for demonstration purposes.</b>
 </div>
@@ -8,5 +8,6 @@
 <style>
     .dummy-alert{
         font-size: 1.5rem;
+        width: 100%;
     }
 </style>
